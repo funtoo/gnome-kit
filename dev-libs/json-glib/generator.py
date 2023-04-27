e3f7c3a5a4be3094fd02912ebf080f1aeb82af14
Daniel Robbins <drobbins@funtoo.org>
2023-04-27 22:13:39 +0000

    Autogenerated tree updates.
    merged:
    
      gnome-kit-sources: 087bacbacaa25fa0e038054ceeaf6f55da08e53d

---
+++ dev-libs/json-glib/generator.py
@@ -0,0 +1,40 @@
+#!/sr/bin/env python3
+
+from packaging.version import Version
+
+async def generate(hub, **pkginfo):
+	name = pkginfo['name']
+	download_url = f"https://download.gnome.org/sources/{name}"
+	json_data = await hub.pkgtools.fetch.get_page(f"{download_url}/cache.json", is_json=True)
+	releases = [Version(v) for v in json_data[2][name]]
+
+	stable = pkginfo.get('stable')
+	if stable:
+		filtered = [v for v in releases if not v.minor & 1]
+	else:
+		filtered = releases
+
+	target = pkginfo.get('version')
+	if target:
+		target = Version(target)
+		version = max([v for v in filtered if v.major == target.major and v.minor == target.minor])
+	else:
+		version = max(filtered)
+
+
+	# Update the pkginfo version with the actual version
+	pkginfo['version'] = version
+
+	release = json_data[1][name][version.public]
+	tarpath = [v for k, v in release.items() if 'tar' in k][0]
+	tarball = tarpath.split("/")[1]
+	artifact = hub.pkgtools.ebuild.Artifact(url=f"{download_url}/{tarpath}", final_name=tarball)
+	
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		artifacts=[artifact]
+	)
+	ebuild.push()
+
+
+# vim: ts=4 sw=4 noet
